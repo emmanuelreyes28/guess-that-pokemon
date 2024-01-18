971c7d9c34d2e34c3329554bd5c58ec914487950
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2024-01-17 23:13:18 -0800

    add TargetPokemon type and fetchPokemonOptions function

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,22 +1,62 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import PokemonDetails from "../PokemonDetails";
 
+//create types for states
+export type TargetPokemon = {
+  name: string;
+  sprite: string;
+};
+
+const randomId = (): number => {
+  return Math.floor(Math.random() * 151) + 1;
+};
+
 export default function Test() {
+  //create states for target pokemon and pokemon names
+  const [targetPokemon, setTargetPokemon] = useState<TargetPokemon | null>(
+    null
+  );
+
   useEffect(() => {
     async function fetchPokemonTarget() {
       try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/1");
+        const id = randomId();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
         const data = await response.json();
         const sprite = data.sprites.front_default;
-        console.log(sprite);
+        const pokemonName = data.name;
+        setTargetPokemon({ name: pokemonName, sprite: sprite });
       } catch (error) {
         console.log(error);
       }
     }
+
+    //create fetchPokemonNames async functions
     fetchPokemonTarget();
+
+    async function fetchPokemonOptions() {
+      try {
+        const pokemonNames: string[] = [];
+        for (let i = 0; i < 3; i++) {
+          const id = randomId();
+          const response = await fetch(
+            `https://pokeapi.co/api/v2/pokemon/${id}`
+          );
+          const data = await response.json();
+          const pokemonName = data.name;
+          pokemonNames.push(pokemonName);
+        }
+        console.log(pokemonNames);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchPokemonOptions();
   }, []);
 
+  //pass in state values into to pokemonDetails as props
   return <PokemonDetails />;
 }
